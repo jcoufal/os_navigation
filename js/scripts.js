620bbc72d082cc8589ee23bb7603008833a0c35a
Radomir Dopieralski <openstack@sheep.art.pl>
2013-09-16 11:18:04 +0200

    Move scripts to a separate file

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(function () {
+    var activate = function (target) {
+        target.siblings().hide();
+        target.show();
+    };
+
+    // Dropdowns activated on click
+    $('.dropdown:not(.dropdowns-hover) .dropdown_trigger').click(function () {
+        $(this).next('.dropdown_menu').toggle();
+    });
+
+    // Handle active class in dropdowns
+    $('.dropdown > ul > li').click(function () {
+        $(this).closest('.dropdown_menu').hide();
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Hide all elements that are not selected in a dropdown
+    $('.dropdown > ul > li.active').each(function () {
+        var target = $($(this).find('a').attr('href'));
+        activate(target);
+    });
+
+    // Handle active status of navigation tabs
+    $('.nav_bar li').click(function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Hide all elements that are not selected in a navigation
+    $('.nav_bar li.active').each(function () {
+        var target = $($(this).find('a').attr('href'));
+        activate(target);
+    });
+
+    // Handle links to elements
+    $('a[href!="#"]').click(function () {
+        var target = $($(this).attr('href'));
+        activate(target);
+    });
+
+    // Start with empty content
+    $('#content > *').hide();
+});
